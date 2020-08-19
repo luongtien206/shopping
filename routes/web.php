9ccc08d856f053c22ff384663f8169c22b04e809
luongtien206 <luongtien.dev@gmail.com>
2020-08-19 11:50:33 +0700

    học laravel phần shopping

--- routes/web.php
+++ routes/web.php
@@ -7,3 +7,14 @@ Route::get('/', function () {
 Route::get('/home', function () {
     return view('home');
 });
+
+Route::prefix('categories')->group(function () {
+    Route::get('/index',[
+        'as'=> 'categories.index',
+        'uses'=>'CategoryController@index'
+    ] );
+    Route::get('/create',[
+        'as'=> 'categories.create',
+        'uses'=>'CategoryController@create'
+    ] );
+});
